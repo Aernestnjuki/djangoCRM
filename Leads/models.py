cb29f735d27f93af7c280373ed22877cb9f55f0a
Aernestnjuki <74731728+Aernestnjuki@users.noreply.github.com>
2025-02-09 23:18:38 +0300

    added more models

--- Leads/models.py
+++ Leads/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 TYPE_OF_COMMUNICATION = (
     ('Phone Call', 'Phone Call'),
@@ -15,8 +16,23 @@ LEAD_SOURCE = (
     ('Referral', 'Referral'),
 )
 
+class User(AbstractUser):
+    email = models.EmailField(unique=True, max_length=200)
+
+    REQUIRED_FIELDS = ['username']
+    USERNAME_FIELD = 'email'
+
+    def __str__(self):
+        return self.email
+    
+class Agent(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    agent_name = models.CharField(max_length=100)
+
+
 class Lead(models.Model):
-    full_name = models.CharField(max_length=100)
+    agent = models.ForeignKey(Agent, on_delete=models.PROTECT)
+    lead_name = models.CharField(max_length=100)
     phone_number = models.IntegerField()
     comment = models.TextField()
     mode_of_communication = models.CharField(max_length=100, choices=TYPE_OF_COMMUNICATION)
